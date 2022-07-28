7f12eba72101ab58800bbdfb104f65e456166349
Z0L3S <raneln01@gmail.com>
2022-07-28 05:52:42 +0000

    Changes Config

--- scripts/feeds_l.sh
+++ scripts/feeds_l.sh
@@ -2,3 +2,5 @@
 
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git 3ginfo_lite https://github.com/4IceG/luci-app-3ginfo-lite' feeds.conf.default
+sed -i '$a src-git modemband https://github.com/4IceG/luci-app-modemband' feeds.conf.default
