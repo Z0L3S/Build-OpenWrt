84ee364248affdd8048659b3ff0fab674d3f1450
Z0L3S <93368816+Z0L3S@users.noreply.github.com>
2022-11-12 12:53:23 +0800

    Update feeds_o.sh

--- scripts/feeds_o.sh
+++ scripts/feeds_o.sh
@@ -4,3 +4,5 @@ sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i '$a src-git 3ginfo_lite https://github.com/4IceG/luci-app-3ginfo-lite' feeds.conf.default
 sed -i '$a src-git modemband https://github.com/4IceG/luci-app-modemband' feeds.conf.default
+sed -i '$a src-git sms-tool https://github.com/4IceG/luci-app-sms-tool' feeds.conf.default
+sed -i '$a src-git atinout-mod https://github.com/4IceG/luci-app-atinout-mod' feeds.conf.default
